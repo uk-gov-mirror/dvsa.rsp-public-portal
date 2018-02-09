b70a993b4eddeec028583abec83d8cd6e523e592
Fabio Simao <fabio.simao@bjss.com>
2018-02-09 14:53:00 +0000

    Adds npm script to facilitate development Adds rudimentar validation checks for the payment code Adds validation errors to the front-end

--- src/server/controllers/payment.controller.js
+++ src/server/controllers/payment.controller.js
@@ -8,28 +8,33 @@ export const robots = (req, res) => {
 
 // Index Route
 export const index = (req, res) => {
-  const viewData = {
-    pageBreadcrumbItems: [
-      { text: 'Home', url: '/' },
-      { text: 'Payment Code', url: '#' },
-    ],
-  };
-
-  res.render('payment/index', viewData);
+  res.render('payment/index');
 };
 
-const paymentCodeValidationChecks = [
-  body('payment-code').trim().not().isEmpty(),
+// Removes all non-alphanumeric characters and converts to lowercase
+export const normalizePaymentcode = (req, res, next) => {
+  req.body['payment-code'] = req.body['payment-code'].replace(/\W|_/g, '').toLowerCase();
+  next();
+};
+
+export const paymentCodeValidationChecks = [
+  body('payment-code').isLength({ min: 16, max: 16 }),
   body('payment-code').trim().isHexadecimal(),
 ];
 
 export const validatePaymentCode = [
+  normalizePaymentcode,
   paymentCodeValidationChecks,
   (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.render('payment/index', { invalid: true });
+      const viewData = {
+        invalidPaymentCode: true,
+      };
+      res.render('payment/index', viewData);
+    } else {
+      res.redirect(`payment-code/${req.body['payment-code']}`);
     }
   },
 ];
