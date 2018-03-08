ae393c16d0b6d870821b9738800bfed7bd990ab1
fabiosimaobjss <35725305+fabiosimaobjss@users.noreply.github.com>
2018-03-08 12:33:11 +0000

    Changes penalty service so it uses the ID to infer the penalty reference (#9)
    Updates unit tests

--- test/utils/mockedBackendAPI.js
+++ test/utils/mockedBackendAPI.js
@@ -8,20 +8,14 @@ export default class MockedBackEndAPI {
     const result = this.penalties.find(p => p.paymentToken === code);
 
     if (result) {
-      return Promise.resolve({ data: { Value: result } });
+      return Promise.resolve({
+        data: {
+          Value: result,
+          ID: `${result.referenceNo}_${result.penaltyType}`,
+        },
+      });
     }
 
     return Promise.reject(new Error('Invalid Payment Code'));
-  }
-
-  getPenaltyByReference(requestUrl) {
-    const reference = requestUrl.split('/').pop();
-    const result = this.penalties.find(p => p.referenceNo === reference);
-
-    if (result) {
-      return Promise.resolve({ data: { Value: result } });
-    }
-
-    return Promise.reject(new Error('Invalid Penalty Reference'));
   }
 }
