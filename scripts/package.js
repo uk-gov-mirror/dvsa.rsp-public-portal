c624136f052affd8e7284d1eb87c51dec1104e4a
Fabio Simao <fabio.simao@bjss.com>
2018-04-25 11:21:07 +0100

    Adds i18n directory to the package Fixes issue causing the app to break on start up (on cloud environment)

--- scripts/package.js
+++ scripts/package.js
@@ -9,6 +9,7 @@ const outputArtifact = path.join(outputPath, 'package.zip');
 const handlerPath = path.join(outputPath, 'handler.js');
 const assetsPath = path.join(outputPath, 'public');
 const viewsPath = path.join(outputPath, 'views');
+const languagesPath = path.join(outputPath, 'i18n');
 
 const output = fs.createWriteStream(outputArtifact);
 const archive = archiver('zip', {
@@ -21,6 +22,6 @@ archive.append(fs.readFileSync(handlerPath), { name: 'handler.js' });
 archive.directory(nodeModulesPath, 'node_modules');
 archive.directory(assetsPath, 'public');
 archive.directory(viewsPath, 'views');
-
+archive.directory(languagesPath, 'i18n');
 archive.finalize();
 
