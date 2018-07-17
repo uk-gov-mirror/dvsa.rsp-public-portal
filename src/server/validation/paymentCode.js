de5a10c46e167689211b5c003d52bf7a9e0eaf41
Ammar Haider <ammar.haider@bjss.com>
2018-07-17 15:30:04 +0100

    removes validation for single token

--- src/server/validation/paymentCode.js
+++ src/server/validation/paymentCode.js
@@ -1,7 +1,6 @@
 import { check } from 'express-validator/check';
 
 export default [
-  // TODO: adjust min to allow for multi penalty payment group
-  check('payment_code').isLength({ min: 16, max: 16 }),
-  check('payment_code').trim().isHexadecimal(),
+  check('payment_code').isLength({ min: 5, max: 16 }),
+  // check('payment_code').trim().isHexadecimal(),
 ];
