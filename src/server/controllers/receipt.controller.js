b58a9058522386583d31124d46cffddad118b44e
John Morley <john.morley@bjss.com>
2018-08-07 17:28:15 +0100

    Call payment service for multi-penalty payment receipt info
    * Happy path of fetching payment information and rendering it into the
      multiPaymentReceipt template
    * No error handling at this point

--- src/server/controllers/receipt.controller.js
+++ src/server/controllers/receipt.controller.js
@@ -1,14 +1,17 @@
 import config from '../config';
 import PenaltyGroupService from '../services/penaltyGroup.service';
+import PaymentService from '../services/payment.service';
 
 const penaltyGroupService = new PenaltyGroupService(config.penaltyServiceUrl);
+const paymentService = new PaymentService(config.paymentServiceUrl);
 
 export default async (req, res) => {
   const paymentCode = req.params.payment_code;
   const penaltyGroup = await penaltyGroupService.getByPenaltyGroupPaymentCode(paymentCode);
-  console.log(JSON.stringify(penaltyGroup));
+  const paymentDetails = (await paymentService.getGroupPayment(paymentCode)).data;
   const resp = {
     paymentType: req.params.type,
+    paymentDetails,
     ...penaltyGroup,
   };
   res.render('payment/multiPaymentReceipt', resp);
--- test/controllers/receipt.controller.spec.js
+++ test/controllers/receipt.controller.spec.js
@@ -2,9 +2,12 @@ import sinon from 'sinon';
 
 import PenaltyGroupService from '../../src/server/services/penaltyGroup.service';
 import ReceiptController from '../../src/server/controllers/receipt.controller';
+import PaymentService from '../../src/server/services/payment.service';
 
 describe('ReceiptController', () => {
   let penaltyGroupSvc;
+  let paymentSvc;
+
   const request = { params: { payment_code: 'abcdefghij1', type: 'FPN' } };
   let redirectSpy;
   let renderSpy;
@@ -29,7 +32,7 @@ describe('ReceiptController', () => {
         }
       ]
     },
-    paymentCode: '15xr4z4ql4v4',
+    paymentCode: 'abcdefghij1',
     penaltyDetails: [
       {
         type: 'FPN',
@@ -102,23 +105,45 @@ describe('ReceiptController', () => {
       ]
     }
   };
+
+  const groupPaymentResp = {
+    ID: 'abcdefghij1',
+    Payments: {
+      IM: {
+        PaymentRef: 'RJF12345',
+        AuthCode: '1234TBD',
+        PaymentAmount: '80',
+        PaymentDate: 1519300376667,
+        PaymentStatus: "PAID",
+      }
+    }
+  };
   beforeEach(() => {
     penaltyGroupSvc = sinon.stub(PenaltyGroupService.prototype, 'getByPenaltyGroupPaymentCode');
     penaltyGroupSvc
       .withArgs('abcdefghij1')
       .resolves(penaltyGroupSvcResp);
+    paymentSvc = sinon.stub(PaymentService.prototype, 'getGroupPayment');
+    paymentSvc
+      .withArgs('abcdefghij1')
+      .resolves({ data: groupPaymentResp })
     redirectSpy = sinon.spy();
     renderSpy = sinon.spy();
     response = { redirect: redirectSpy, render: renderSpy };
   });
   afterEach(() => {
     PenaltyGroupService.prototype.getByPenaltyGroupPaymentCode.restore();
+    PaymentService.prototype.getGroupPayment.restore();
     redirectSpy.resetHistory();
     renderSpy.resetHistory();
   });
 
   it('should render the penalty group into the receipt view', async () => {
     await ReceiptController(request, response);
-    sinon.assert.calledWith(renderSpy, 'payment/multiPaymentReceipt', {paymentType: 'FPN', ...penaltyGroupSvcResp});
+    sinon.assert.calledWith(renderSpy, 'payment/multiPaymentReceipt', {
+      paymentType: 'FPN',
+      paymentDetails: groupPaymentResp,
+      ...penaltyGroupSvcResp
+    });
   });
 });
