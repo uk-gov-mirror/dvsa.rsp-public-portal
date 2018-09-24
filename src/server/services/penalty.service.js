c1fe726a9f072308deadcd1f11f53ac5c1053bea
John Morley <john.morley@bjss.com>
2018-09-24 16:39:29 +0100

    Prevent display of penalty group details where cancelled

--- src/server/services/penalty.service.js
+++ src/server/services/penalty.service.js
@@ -47,7 +47,7 @@ export default class PenaltyService {
   getByPaymentCode(paymentCode) {
     const promise = new Promise((resolve, reject) => {
       this.httpClient.get(`documents/tokens/${paymentCode}`).then((response) => {
-        if (isEmpty(response.data) || response.data.Enabled === false) {
+        if (isEmpty(response.data)) {
           reject(new Error('Payment code not found'));
         }
         resolve(PenaltyService.parsePenalty(response.data));
