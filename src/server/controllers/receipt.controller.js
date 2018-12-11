849ad9a1b31ac81f16702177437b06db4afc316f
James Highfield <James.Highfield@bjss.com>
2018-12-11 17:45:06 +0000

    Add single penalty receipt

--- src/server/controllers/receipt.controller.js
+++ src/server/controllers/receipt.controller.js
@@ -1,16 +1,79 @@
-/* eslint-disable no-use-before-define */
-
+// @ts-check
 import moment from 'moment-timezone';
 
 import config from '../config';
 import PenaltyGroupService from '../services/penaltyGroup.service';
 import PaymentService from '../services/payment.service';
+import PenaltyService from '../services/penalty.service';
+
+const TIMEZONE_ID = 'Europe/London';
 
 const penaltyGroupService = new PenaltyGroupService(config.penaltyServiceUrl());
+const penaltyService = new PenaltyService(config.penaltyServiceUrl());
 const paymentService = new PaymentService(config.paymentServiceUrl());
 
-export default async (req, res) => {
-  console.log('receipt page');
+function addFormattedPamentDateTime(payment) {
+  const timestamp = payment.PaymentDate * 1000;
+  const dateTime = moment.tz(timestamp, TIMEZONE_ID);
+  return {
+    FormattedDate: dateTime.format('DD/MM/YYYY'),
+    FormattedTime: dateTime.format('h:mma'),
+    ...payment,
+  };
+}
+
+function paymentDetailsFromPenalty(penalty, payment) {
+  let paymentDetail = {
+    PaymentAmount: penalty.amount,
+    PaymentStatus: penalty.status,
+    ...payment.PaymentDetail,
+  };
+
+  paymentDetail = addFormattedPamentDateTime(paymentDetail);
+
+  return {
+    Payments: { [penalty.type]: paymentDetail },
+  };
+}
+
+function isValidPaymentPaymentType(type) {
+  return ['FPN', 'CDN', 'IM'].includes(type);
+}
+
+function addFormattedPaymentDateTimes(paymentDetails) {
+  const newPaymentDetails = { ...paymentDetails };
+  newPaymentDetails.Payments = Object.keys(newPaymentDetails.Payments).reduce((acc, type) => {
+    acc[type] = addFormattedPamentDateTime(newPaymentDetails.Payments[type]);
+    return acc;
+  }, {});
+  return newPaymentDetails;
+}
+
+export const singlePaymentReceipt = async (req, res) => {
+  const paymentCode = req.params.payment_code;
+  try {
+    const penalty = await penaltyService.getByPaymentCode(paymentCode);
+    const paymentId = `${penalty.formattedReference}_${penalty.type}`;
+    const { payment } = (await paymentService.getPayment(paymentId)).data;
+    const paymentDetails = paymentDetailsFromPenalty(penalty, payment);
+
+    const resp = {
+      paymentType: penalty.type,
+      paymentStatus: penalty.status,
+      paymentDetails,
+      registrationNumber: penalty.vehicleReg,
+      location: penalty.location,
+      date: penalty.issueDate,
+      penaltyDetails: [{ type: penalty.type, penalties: [penalty] }],
+      paymentCode,
+    };
+    return res.render('payment/multiPaymentReceipt', resp);
+  } catch (error) {
+    return res.redirect(`${config.urlRoot()}/?invalidPaymentCode`);
+  }
+};
+
+export const multiPaymentReceipt = async (req, res) => {
   try {
     const paymentCode = req.params.payment_code;
     const { type } = req.params;
@@ -23,32 +86,17 @@ export default async (req, res) => {
     const paymentDetails = (await paymentService.getGroupPayment(paymentCode)).data;
     const enrichedPaymentDetails = addFormattedPaymentDateTimes(paymentDetails);
 
+    const { penaltyGroupDetails, ...penaltyGroupData } = penaltyGroup;
+
     const resp = {
       paymentType: req.params.type,
       paymentDetails: enrichedPaymentDetails,
-      ...penaltyGroup,
+      ...penaltyGroupData,
+      ...penaltyGroupDetails,
     };
+
     return res.render('payment/multiPaymentReceipt', resp);
   } catch (error) {
     return res.redirect(`${config.urlRoot()}/?invalidPaymentCode`);
   }
 };
-
-function isValidPaymentPaymentType(type) {
-  return ['FPN', 'CDN', 'IM'].includes(type);
-}
-
-function addFormattedPaymentDateTimes(paymentDetails) {
-  const newPaymentDetails = { ...paymentDetails };
-  newPaymentDetails.Payments = Object.keys(newPaymentDetails.Payments).reduce((acc, type) => {
-    const timestamp = newPaymentDetails.Payments[type].PaymentDate * 1000;
-    const timezoneId = 'Europe/London';
-    acc[type] = {
-      FormattedDate: moment.tz(timestamp, timezoneId).format('DD/MM/YYYY'),
-      FormattedTime: moment.tz(timestamp, timezoneId).format('h:mma'),
-      ...newPaymentDetails.Payments[type],
-    };
-    return acc;
-  }, {});
-  return newPaymentDetails;
-}
